4d2ab72356997b8c977d0a5f342e8a5dfce33bc0
Hionel <104563991+Hionel@users.noreply.github.com>
2023-02-20 18:43:20 +0200

    firebase linked to app

--- auth/src/app/app.module.ts
+++ auth/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PanelComponent } from './components/panel/panel.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    PanelComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ auth/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'authtesting-f617f',
+    appId: '1:770230861320:web:71f1778d5550eb4c131bbd',
+    storageBucket: 'authtesting-f617f.appspot.com',
+    apiKey: 'AIzaSyDdlJcEiw7qHnFV3zEQ9Vk4NBBezP1vy28',
+    authDomain: 'authtesting-f617f.firebaseapp.com',
+    messagingSenderId: '770230861320',
+  },
+  prod: false,
+};
